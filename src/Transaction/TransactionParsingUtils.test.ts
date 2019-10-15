e726dbc49d0fdb4c52a60023f72e16f8bd53fc6f
Noel Ray Barron <noelraybarron@hotmail.com>
2019-10-14 22:42:56 -0700

    Add test to inferTransactionName

--- src/Transaction/TransactionParsingUtils.test.ts
+++ src/Transaction/TransactionParsingUtils.test.ts
@@ -5,6 +5,7 @@ import {
 } from "../Accounts/AccountStore";
 import {
     inferAccount,
+    inferTransactionName,
     parseDate,
 } from "./TransactionParsingUtils";
 
@@ -53,6 +54,57 @@ test("Tets inferAccount for all known accoun", () => {
     };
     Object.keys(accountMap).forEach((accountName) => {
         expect(inferAccount(accountName)).toBe(accountMap[accountName]);
+    });
+});
+
+test("Tets inferTransactionName for known descriptions from RBC", () => {
+    const testAssetMap: {[key: string]: string} = {
+        "7-ELEVEN STORE #37875 SURREY BC": "7-ELEVEN",
+        "ADOBE *PRODUCTS 8008336687 CA": "ADOBE",
+        "AMATO GELATO CAFE VANCOUVER BC": "AMATO GELATO CAFE",
+        "AMZN Mktp CA*K316I0K43 WWW.AMAZON.CAON": "AMAZON",
+        "APL*ITUNES.COM/BILL 866-712-7753 ON": "ITUNES",
+        "Audible CA*MO9HQ6X40 Amzn.com/billNJ": "AUDIBLE",
+        "BEST BUY #961 SURREY BC": "BEST BUY",
+        "BISTRO SAKANA JAPANESE VANCOUVER BC": "BISTRO SAKANA",
+        "BUBBLE WAFFLE CAFE VANCOUVER BC": "BUBBLE WAFFLE",
+        "CDN TIRE STORE #00489 SURREY BC": "CANADIAN TIRE",
+        "CHATIME SURREY BC": "CHATIME",
+        "COMPASS AUTOLOAD 604-398-2042 BC": "TRANSLINK",
+        "COSTCO WHOLESALE W51 BURNABY BC": "COSTCO",
+        "CRAFT BEER MARKET - VA VANCOUVER BC": "CRAFT BEER MARKET",
+        "DELICIOUS PHO VANCOUVER BC": "DELICIOUS PHO",
+        "FALAFEL KING VANCOUVER BC": "FALAFEL KING",
+        "FAMOUS PLAYER 1412QPS COQUITLAM BC": "CINEPLEX",
+        "FIDO Mobile *696794023 888-481-3436 ON": "FIDO",
+        "IKEA COQUITLAM COQUITLAM BC": "IKEA",
+        "INDIGO 975 VANCOUVER BC": "CHAPTERS",
+        "KITANOYA GUU GARDEN VANCOUVER BC": "GUU GARDEN",
+        "LANDMARK 10 GUILDFORD SURREY BC": "LANDMARK",
+        "LANDMARK WEB TICKETING 403-262-4255 AB": "LANDMARK",
+        "MAN CAVE BARBER SHOP VANCOUVER BC": "MAN CAVE",
+        "MCDONALD'S #40354 VANCOUVER BC": "MCDONALDS",
+        "NEW SZECHUAN RESTAURAN SURREY BC": "NEW SZECHUAN RESTAURANT",
+        "OPA066-GUILDFORD TOWN SURREY BC": "OPA",
+        "PAYMENT - THANK YOU / PAIEMENT - MERCI": "RBC MASTERCARD",
+        "PETROCAN SURREY BC": "PETROCAN",
+        "SIMON FRASER UNIVERSIT BURNABY BC": "SFU",
+        "STARBUCKS 04468 SURREY BC": "STARBUCKS",
+        "STARBUCKS 04941 SURREY BC": "STARBUCKS",
+        "STARBUCKS 21565 SURREY BC": "STARBUCKS",
+        "T&T SUPERMARKET #003 VANCOUVER BC" : "T&T",
+        "TACO TIME PACIFIC CENT VANCOUVER BC": "TACO TIME",
+        "THIRST FIRST REFRESHME VANCOUVER BC": "WORK",
+        "TIM HORTONS #3190 VANCOUVER BC": "TIM HORTONS",
+        "TIM HORTONS #6868 SURREY BC": "TIM HORTONS",
+        "TIM HORTONS Toronto ON": "TIM HORTONS",
+        "VIRGIN MOBILE VERDUN QC": "VIRGIN MOBILE",
+        "WAL*MART CANADA INC Mississauga ON": "WALMART",
+        "WAL-MART #5838 SURREY BC": "WALMART",
+        "YAH-YAH-YA VANCOUVER BC": "YAH-YAH-YA",
+    };
+    Object.keys(testAssetMap).forEach((rawDescription) => {
+        expect(inferTransactionName(rawDescription)).toBe(testAssetMap[rawDescription]);
     });
 });
 
